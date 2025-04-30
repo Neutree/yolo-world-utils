bfd03778a76e6dfb423c006de04162de2f9f209f
Neucrack <CZD666666@gmail.com>
2025-04-30 18:39:44 +0800

    optimize download

---
+++ README.md
@@ -0,0 +1,11 @@
+yolo-world-utils
+===
+
+## Usage
+
+### generate text feature
+
+```
+echo person > my_labels.txt
+python -u -m yolo_world_utils gen_text_feature --labels_path my_labels.txt --out_feature_path my_labels.bin
+```
--- yolo_world_utils/__init__.py
+++ yolo_world_utils/__init__.py
@@ -4,19 +4,25 @@ onnx_path = os.path.join(os.path.dirname(__file__), "yoloworld.vitb.txt.onnx")
 if not os.path.exists(onnx_path):
     print(f"now downloading {os.path.basename(onnx_path)} file, please wait")
     print(f"you can mannually download it (https://github.com/Neutree/yolo-world-utils/releases) and put it in {onnx_path}")
+    msg = f"Download {os.path.basename(onnx_path)} failed, you can mannually download from https://github.com/Neutree/yolo-world-utils/releases\n"
+    msg += f"And save to {onnx_path}"
+    error = False
     try:
-        from huggingface_hub import hf_hub_download
-        import shutil
-        file_path = hf_hub_download(
-            repo_id="neucrack/yolo-world-utils",
-            filename="yolo_world_utils/yoloworld.vitb.txt.onnx",
-            revision="main"
-        )
-        shutil.move(file_path, onnx_path)
-        del hf_hub_download, file_path
+        import requests
+        url = "https://github.com/Neutree/yolo-world-utils/releases/download/v1.0.0/yoloworld.vitb.txt.onnx"
+
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(onnx_path, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        else:
+            print("Downlaod faield:", response.status_code)
+            error=True
     except Exception as e:
         print(e)
-        print(f"Download {os.path.basename(onnx_path)} failed, you can mannually download from https://github.com/Neutree/yolo-world-utils/releases")
-        print(f"And save to {onnx_path}")
+        error = True
+    if error:
+        raise Exception(msg)
 
 
